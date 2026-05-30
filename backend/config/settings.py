c9668373d7a623884448cb05ce1c633eb743553d
uzieltzab <uzielruxtion@gmail.com>
2026-05-30 17:35:32 -0600

    fix: reemplace config with os.environ

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 ASGI_APPLICATION = 'config.asgi.application'
 
 # WebSockets Channel Layer
-if not DEBUG or config('REDIS_URL', default=False):
+if not DEBUG or os.environ.get('REDIS_URL'):
     CHANNEL_LAYERS = {
         "default": {
             "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                "hosts": [config('REDIS_URL', default='redis://127.0.0.1:6379/0')],
+                "hosts": [os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0')],
             },
         },
     }
